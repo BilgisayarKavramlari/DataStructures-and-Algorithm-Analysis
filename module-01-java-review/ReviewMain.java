f96b092bcfe97ccbf856813a05beaeb00e6607dc
Sadi Evren SEKER <bk@shedai.net>
2026-06-24 17:31:44 +0000

    Remove all files

--- module-01-java-review/ReviewMain.java
+++
@@ -1,8 +0,0 @@
-public class ReviewMain {
-    public static void main(String[] args) {
-        // A Java program starts running in the main method.
-        System.out.println("Welcome to CS 3345 Java examples!");
-        System.out.println("Compile with: javac ReviewMain.java");
-        System.out.println("Run with: java ReviewMain");
-    }
-}
